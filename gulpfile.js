9ea24489c99c15c65ab64ac713511fec8a437c29
Tijs <tijsvervoort@gmail.com>
2017-06-09 15:34:18 +0200

    template for image with text

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('browser-sync', ['sass'], function () {
             //domain: 'localhost:3000'
             // For external development (e.g on a mobile or tablet) use an external URL.
             // You will need to update this to whatever BS tells you is the external URL when you run Gulp.
-            domain: 'localhost:3000'
+            domain: 'localhost:8080'
         }
     });
 });
